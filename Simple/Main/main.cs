1cd486fde7beba493585e865d14726cea37cae43
alexey.babich <alexey.babich@apriorit.com>
2019-08-01 16:54:43 +0300

    initial commit

---
+++ Simple/Main/main.cs
@@ -0,0 +1,14 @@
+using System;
+using NecessaryDll;
+
+namespace Samples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Work();
+        }
+    }
+}
